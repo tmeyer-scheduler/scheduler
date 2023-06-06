e370cf88ff68fb3fb8b8a5ab74cf73a28b561464
Thomas Meyer <thomasmmeyer88@gmail.com>
2023-06-05 20:40:12 -0500

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tmeyer-scheduler.github.io';
   showToolsRow = false;
+  showToolsCol = false;
   newRow = {columns: 0, height: 0, width: 100, bgcolor: 'green' };
 
+  currentElement: any;
   showRowOptions() {
     this.showToolsRow = !this.showToolsRow;
   }
@@ -22,18 +24,42 @@ export class AppComponent {
         for (let i = 0; i < this.newRow.columns; i++) {
           
           let newCol = document.createElement('div');
+          let $ = this;
           newRow.appendChild(newCol);
           newCol.textContent = "Click to Edit";
-          newCol.addEventListener("click", this.editThis);
           newCol.classList.add(`col-${this.newRow.columns}`, `newEle`);
+          newCol.addEventListener('click', function() {
+            $.currentElement = this;
+            $.showToolsCol = true;
+          })
         }
         body.appendChild(newRow);
     }
     this.showRowOptions();
   }
 
-  editThis(this: any) {
-    console.log(this);
+  deleteRow() {
+    let ele = this.currentElement as HTMLElement;
+    let row = ele.parentElement as Node;
+    let body = document.getElementById('body');
+    body?.removeChild(row);
   }
 
+  deleteCol() {
+    let ele = this.currentElement as Node;
+    let row = ele.parentElement;
+    row?.removeChild(ele);
+  }
+
+  closeToolsCol() {
+    this.showToolsCol = false;
+  }
+
+  alignLeft() {
+
+  }
+
+  alignRight() {
+    
+  }
 }
