2a60ba68af4958b904acb190b4cd2a3548b1714a
Thomas Meyer <thomasmmeyer88@gmail.com>
2023-06-05 19:43:54 -0500

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tmeyer-scheduler.github.io';
+  showToolsRow = false;
+  newRow = {columns: 0, height: 0, width: 100, bgcolor: 'green' };
+
+  showRowOptions() {
+    this.showToolsRow = !this.showToolsRow;
+  }
+
+  addRow() {
+    let body = document.getElementById('body');
+    if (body != null) {
+        let newRow = document.createElement('div');
+        newRow.classList.add('row');
+        for (let i = 0; i < this.newRow.columns; i++) {
+          
+          let newCol = document.createElement('div');
+          newRow.appendChild(newCol);
+          newCol.textContent = "Click to Edit";
+          newCol.addEventListener("click", this.editThis);
+          newCol.classList.add(`col-${this.newRow.columns}`, `newEle`);
+        }
+        body.appendChild(newRow);
+    }
+    this.showRowOptions();
+  }
+
+  editThis(this: any) {
+    console.log(this);
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { CreateAccountComponent } from './create-account/create-account.componen
 import { SignInComponent } from './sign-in/sign-in.component';
 import { ProfileComponent } from './profile/profile.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,9 +28,15 @@ import { ProfileComponent } from './profile/profile.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ }
